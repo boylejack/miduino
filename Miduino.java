435c1ca22f0fede5089091bdb4ba6b7d7ea93a24
mycroftHo <boylej4@tcd.ie>
2015-04-21 19:08:34 +0100

    fully reading file header and reading length of track chunk

--- Miduino.java
+++ Miduino.java
@@ -3,9 +3,17 @@ import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.io.IOException;
 
+/*
+    Miduino is a translator from Midi to Arduino code.
+    It is designed to be used in conjunction with Jack Boyle's arduino midi
+    sequencer project. An instructable for said project can be found at ____
+
+*/
 public class Miduino{
     private static int inputMarker;
+    private static int vTime;
     private static int numberOfTracks;
+    private static int tracklength;
     private static byte fileForm;
     private static short division;
     private static byte[] theFile;
@@ -15,12 +23,43 @@ public class Miduino{
             return data;
     }
 
-    public static int timeDiv(){
-        //division += theFile[inputMarker++] << 8;
-        //division += theFile[inputMarker++];
-        //System.out.println("Time division = " + division);
-        System.out.println("first byte = " + theFile[inputMarker++]);
+    private static int time(){
+
+    }
+
+    private static int event(){
+
+    }
+
+    private static int chunkLength(){
+        tracklength += theFile[inputMarker++] << 24;
+        tracklength += theFile[inputMarker++] << 16;
+        tracklength += theFile[inputMarker++] << 8;
+        tracklength += theFile[inputMarker++] & 0xFF;
+        System.out.println("Track length = " + tracklength);
         return 0;
+    }
+
+    public static int midiTrack(){
+        String trackHead = new String(theFile, inputMarker, 4);
+        if(trackHead.compareTo("MTrk") == 0){
+            inputMarker += 4;
+            System.out.println("Reading track");
+            return chunkLength();
+        }
+        else{
+            System.out.print("Error : Expected track chunk");
+            return -1;
+        }
+    }
+
+    public static int timeDiv(){
+        division += theFile[inputMarker++] << 8;
+        division += theFile[inputMarker++] & 0xFF;
+        System.out.println("Time division = " + division);
+        System.out.println("Successfuly Read Midi Header");
+        //the program should now be at the first midi track chunk
+        return midiTrack();
     }
 
     public static int numbrTracks(){
