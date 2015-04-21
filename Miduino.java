ea90e0e3113ee0e163b86b7b8b904b8f065ceb45
mycroftHo <boylej4@tcd.ie>
2015-04-21 16:53:36 +0100

    currently fixing unsigned addition problem

--- Miduino.java
+++ Miduino.java
@@ -4,24 +4,79 @@ import java.nio.file.Path;
 import java.io.IOException;
 
 public class Miduino{
+    private static int inputMarker;
+    private static int numberOfTracks;
+    private static byte fileForm;
+    private static short division;
+    private static byte[] theFile;
     private static byte[] ReadFile(String fp) throws IOException{
             Path path = Paths.get(fp);
             byte[] data = Files.readAllBytes(path);
             return data;
     }
-/*
-    public static int starting(){
+
+    public static int timeDiv(){
+        //division += theFile[inputMarker++] << 8;
+        //division += theFile[inputMarker++];
+        //System.out.println("Time division = " + division);
+        System.out.println("first byte = " + theFile[inputMarker++]);
+        return 0;
+    }
+
+    public static int numbrTracks(){
+        numberOfTracks += theFile[inputMarker++] << 8;
+        numberOfTracks += theFile[inputMarker++];
+        System.out.println("Number of tracks = " + numberOfTracks);
+        return timeDiv();
 
     }
-*/
+
+    public static int form(){
+        byte frm = theFile[inputMarker];
+        switch(frm){
+            case 0:
+                fileForm = frm;
+                System.out.println("Single Track Midi File");
+                inputMarker += 1;
+                return numbrTracks();
+            case 1:
+                fileForm = frm;
+                System.out.println("Multitrack Midi File");
+                inputMarker += 1;
+                return numbrTracks();
+            case 2:
+                fileForm = frm;
+                System.out.println("Multisong Midi File");
+                inputMarker += 1;
+                return numbrTracks();
+            default:
+                return -1;
+        }
+
+    }
+
+    public static int starting(){
+        String header = new String(theFile, inputMarker, 4);
+        if(header.compareTo("MThd") == 0){
+            System.out.println("This is a midi file");
+            //we skip the 0006 part of the midi header, this is always the same and the first byte of the form bytes.
+            inputMarker += 9;
+            return form();
+        }
+        else{
+            return -1;
+        }
+
+
+    }
+
     public static void main(String [] args){
+        inputMarker = 0;
         String filePath = args[0];
         System.out.println(filePath);
         try{
-            byte[] theFile = ReadFile(filePath);
-            for(int i = 0; i < theFile.length; i++){
-                System.out.print((char) theFile[i]);
-            }
+            theFile = ReadFile(filePath);
+            starting();
         }
         catch(IOException e){
             e.printStackTrace();
