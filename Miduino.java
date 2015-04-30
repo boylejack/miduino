06989b77f7fbb49f1700acd62f1e9de98c84b6c9
mycroftHo <boylej4@tcd.ie>
2015-04-30 23:59:22 +0100

    reads whole midi file, problems with variable length reading

--- Miduino.java
+++ Miduino.java
@@ -7,37 +7,132 @@ import java.io.IOException;
     Miduino is a translator from Midi to Arduino code.
     It is designed to be used in conjunction with Jack Boyle's arduino midi
     sequencer project. An instructable for said project can be found at ____
+    Miduino is released under the ____ licence.
 
 */
 public class Miduino{
     private static int inputMarker;
-    private static int vTime;
+    private static int chunkLength;
+    private static int timeSinceLastEvent;
     private static int numberOfTracks;
     private static int tracklength;
     private static byte fileForm;
     private static short division;
     private static byte[] theFile;
+    
     private static byte[] ReadFile(String fp) throws IOException{
             Path path = Paths.get(fp);
             byte[] data = Files.readAllBytes(path);
             return data;
     }
 
-    private static int time(){
+
+    private static int vchunkLength(){
+	    if(theFile[inputMarker] <= 0x7F && theFile[inputMarker] >= 0){
+		    return vchunkB();
+	    }
+	    else{
+		    return vchunkA();
+	    }
+    }
+
+    //This is not the last byte in the variable length quantity
+    private static int vchunkA(){
+	    System.out.println("At least one more vlq byte to read");
+	    chunkLength = chunkLength << 7;
+	    chunkLength += theFile[inputMarker++] & 0x80;
+	    return vchunkLength();
+    }
+
+    //This is the last byte in the variable length quantity
+    private static int vchunkB(){
+	    chunkLength = chunkLength << 7;
+	    chunkLength += theFile[inputMarker++];
+	    inputMarker += chunkLength;
+	    System.out.println("Length of meta event : " + chunkLength);
+	    return vtime();
 
     }
 
     private static int event(){
+	    int eventType = theFile[inputMarker++] & 0xFF;
+	    if(eventType == 0xFF){
+		    System.out.println("Meta Event");
+		    if(theFile[inputMarker] == 0x2f){
+			    inputMarker += 2;
+			    numberOfTracks -= 1;
+			    //go to next track
+			    System.out.println("End of current track");
+			    if(numberOfTracks == 0){
+				    System.out.println("End of midi file");
+				    return 0;
+			    }
+			    else{
+			    	return midiTrack();
+			    }
+		    }
+		    else{
+			    //skip the event type, we aren't really interested
+			    inputMarker++;
+		    	    chunkLength = 0;
+		   	    return vchunkLength();
+	    	    }
+	    }
+	    else if((eventType & 0xF0) == 0x90){
+		    System.out.println("Note on");
+		    System.out.println("Channel : " + (eventType & 0x0F));
+		    System.out.println("Note : " + (theFile[inputMarker++]));
+		    System.out.println("Velocity : " + (theFile[inputMarker++]));
+		    return vtime();
+	    } 
+	    else if((eventType & 0xF0) == 0x80){
+		    System.out.println("Note off");
+		    System.out.println("Channel : " + (eventType & 0x0F));
+		    System.out.println("Note : " + (theFile[inputMarker++]));
+		    System.out.println("Velocity : " + (theFile[inputMarker++]));
+		    return vtime();
+	    } 
+	    else{
+		    return 0;
+	    }
 
     }
 
+    private static int vtime(){
+	    if(theFile[inputMarker] <= 0x7F && theFile[inputMarker] >= 0){
+		    return vtimeB();
+	    }
+	    else{
+		    return vtimeA();
+	    }
+    }
+
+    //vtimeA represents a value >= 0x80
+    //This is not the last byte in the variable length quantity
+    private static int vtimeA(){
+	    System.out.println("At least one more vlq byte to read");
+	    timeSinceLastEvent = timeSinceLastEvent << 7;
+	    timeSinceLastEvent += theFile[inputMarker++] & 0x80;
+	    return vtime();
+    }
+
+    //vtimeB represents a value < 0x80
+    //This is the last byte in the variable length quantity
+    private static int vtimeB(){
+	    timeSinceLastEvent = timeSinceLastEvent << 7;
+	    timeSinceLastEvent += theFile[inputMarker++];
+	    System.out.println("Time since last event : " + timeSinceLastEvent);
+	    return event();
+
+    }
+    
     private static int chunkLength(){
         tracklength += theFile[inputMarker++] << 24;
         tracklength += theFile[inputMarker++] << 16;
         tracklength += theFile[inputMarker++] << 8;
         tracklength += theFile[inputMarker++] & 0xFF;
         System.out.println("Track length = " + tracklength);
-        return 0;
+        return vtime();
     }
 
     public static int midiTrack(){
@@ -98,7 +193,8 @@ public class Miduino{
         String header = new String(theFile, inputMarker, 4);
         if(header.compareTo("MThd") == 0){
             System.out.println("This is a midi file");
-            //we skip the 0006 part of the midi header, this is always the same and the first byte of the form bytes.
+            //we skip the 0006 part of the midi header, this is always the same 
+	    //we also skip the first byte of the form bytes.
             inputMarker += 9;
             return form();
         }
@@ -111,6 +207,7 @@ public class Miduino{
 
     public static void main(String [] args){
         inputMarker = 0;
+	timeSinceLastEvent = 0;
         String filePath = args[0];
         System.out.println(filePath);
         try{
